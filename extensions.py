62bb51939b8e6da6ecb7c49666a1710623c6ad64
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 00:34:05 +0000

    Sun, Dec 15, 2024, 7:34 PM -05:00

--- extensions.py
+++ extensions.py
@@ -1,2 +1,16 @@
-name = input("File name: ")
+name = input("File name: ").lower()
+if name.endswith(".gif"):
+    print("image/gif")
+elif name.endswith(".jpg") or name.endswith(".jpeg"):
+    print("image/jpeg")
+elif name.endswith(".png"):
+    print("image/png")
+elif name.endswith(".pdf"):
+    print("application/pdf")
+elif name.endswith(".txt"):
+    print("text/plain")
+elif name.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
 
