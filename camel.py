09514b05e7cd2d8cf47e6adc272d6d10ed6cdfa1
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 17:47:16 +0000

    Mon, Dec 16, 2024, 12:47 PM -05:00

--- camel.py
+++ camel.py
@@ -3,7 +3,7 @@
 #Set everything to lower case
 list = []
 str = input("Variable name: ")
-for i in range(len(str) - 1):
+for i in range(len(str)):
     if str[i].isupper():
         list.append(str[0 : i - 1])
         str = str[i : len(str) - 1]
