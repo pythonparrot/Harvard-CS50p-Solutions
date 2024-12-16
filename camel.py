760525db1076817a36f1039642c67477b4ffd4d4
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 18:10:12 +0000

    Mon, Dec 16, 2024, 1:10 PM -05:00

--- camel.py
+++ camel.py
@@ -1,24 +1,14 @@
 #Split the variable name based on upper cases
 str = input("Variable name: ")
-list_indexes = []
+words = []
+start = 0
+
 for i in range(len(str)):
     if str[i].isupper():
-        list_indexes.append(i)
+        words.append(str[start : i])
+        start = i
 
-print(list_indexes)
-
-list_words = []
-
-for i in range(len(list_indexes)):
-    if i == 0:
-        list_words.append(str[0 : list_indexes[i]])
-    if i == (len(list_indexes) - 1):
-        list_words.append(str[list_indexes[i - 1]])
-        list_words.append(str[list_indexes[i]])
-    else:
-        list_words.append(str[list_indexes[i - 1] : list_indexes[i]])
-
-print(list_words)
+print(words)
 
 #Add the word together and put an underscore between each word
 #Set everything to lower case
