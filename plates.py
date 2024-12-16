bf972c4d59fdfd9898b2771ef9ed7018728f8e3d
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 20:29:32 +0000

    Mon, Dec 16, 2024, 3:29 PM -05:00

--- plates.py
+++ plates.py
@@ -10,7 +10,13 @@ def is_valid(s):
     if len(s) >= 2 and len(s) <= 6:
         if s[0 : 2].isalpha():
             if s.isalnum():
-                if 
+                index = 0
+                for i in range(len(s)):
+                    if s[i].isdigit():
+                        index = i
+                        break
+                for i in range
+
 
     print("Invalid")
 
