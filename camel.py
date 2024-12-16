348ff8fd9365fc3485ea3763ab8b13c78a620934
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 17:55:26 +0000

    Mon, Dec 16, 2024, 12:55 PM -05:00

--- camel.py
+++ camel.py
@@ -1,6 +1,14 @@
 #Split the variable name based on upper cases
+str = input("Variable name: ")
+list_indexes = []
+for i in range(len(str)):
+    if str[i].isupper():
+        list_indexes.append(i)
+list_words = []
+
+
 #Add the word together and put an underscore between each word
 #Set everything to lower case
-split
+
 
 
