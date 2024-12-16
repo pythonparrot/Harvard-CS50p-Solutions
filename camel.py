98ee283d74ca89e1d7284550ab874332e244965d
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 18:00:08 +0000

    Mon, Dec 16, 2024, 1:00 PM -05:00

--- camel.py
+++ camel.py
@@ -6,8 +6,10 @@ for i in range(len(str)):
         list_indexes.append(i)
 list_words = []
 for i in range list_indexes:
-    if list_indexes[i] - 1 < 0:
-        
+    if i == 0:
+        list_words.append(str[0 : list_indexes[i]])
+    if i == len()
+
 
 #Add the word together and put an underscore between each word
 #Set everything to lower case
