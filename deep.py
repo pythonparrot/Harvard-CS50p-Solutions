3750ac0925ec893d48dd4a532a5bc9ae706e25ef
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 00:10:22 +0000

    Sun, Dec 15, 2024, 7:10 PM -05:00

--- deep.py
+++ deep.py
@@ -1,4 +1,7 @@
-answer = input("Answer the Great Question of Life, the Universe and Everything: ")
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?: ").lower()
 match answer:
-    case 
+    case "42" | "forty-two" | "forty two":
+        print("Yes")
+    case _:
+        print("No")
 
