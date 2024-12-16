e5a0b8b238e442c600b0ae419995b5da27740508
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 19:12:14 +0000

    Mon, Dec 16, 2024, 2:12 PM -05:00

--- twttr.py
+++ twttr.py
@@ -1,29 +1,13 @@
 str = input("Input: ")
-str1 = str.lower()
 
-while True:
-    if str1.find("a") != -1:
-        i = str1.find("a")
-        str = str[0 : i] + str[i + 1 : ]
-        str1 = str[0 : i] + str[i + 1 : ]
-    elif str1.find("e") != -1:
-        i = str1.find("e")
-        str = str[0 : i] + str[i + 1 : ]
-        str1 = str[0 : i] + str[i + 1 : ]
-    elif str1.find("i") != -1:
-        i = str1.find("i")
-        str = str[0 : i] + str[i + 1 : ]
-        str1 = str[0 : i] + str[i + 1 : ]
-    elif str1.find("o") != -1:
-        i = str1.find("o")
-        str = str[0 : i] + str[i + 1 : ]
-        str1 = str[0 : i] + str[i + 1 : ]
-    elif str1.find("u") != -1:
-        i = str1.find("u")
-        str = str[0 : i] + str[i + 1 : ]
-        str1 = str[0 : i] + str[i + 1 : ]
+def has(string, letter):
+    if string.lower().find(letter.lower()) >= 0:
+        return true
     else:
-        break
+        return false
+
+while str.lower().find("a") >= 0 or str.lower().find("e") >= 0 or str.lower().find("i") >= 0 or str.lower().find("o") >= 0 or str.lower().find("u") >= 0:
+    if
 
 print(str)
 
