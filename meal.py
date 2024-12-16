6ea046f9536a65920e9ab98c044c77d4576a0bdf
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 01:36:51 +0000

    Sun, Dec 15, 2024, 8:36 PM -05:00

--- meal.py
+++ meal.py
@@ -3,7 +3,7 @@ def main():
     time = convert(time)
     if (time >= 7 and time <= 8):
         print("breaakfast time")
-    elif (time >= 12 and <= 13):
+    elif (time >= 12 and time <= 13):
         print("lunch time")
     elif (time >= 18 and time <= 19):
         print("dinner time")
