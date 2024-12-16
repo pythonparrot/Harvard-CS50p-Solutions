ad4a48e4724e17a9d2703a501e2fcfd869c91201
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 18:45:13 +0000

    Mon, Dec 16, 2024, 1:45 PM -05:00

--- coke.py
+++ coke.py
@@ -3,8 +3,8 @@ total = 0
 while True:
     total += int(input("Insert coin: "))
     if total < 50:
-        print("Amount due: " + (50 - total))
+        print("Amount due: " + str(50 - total))
     else:
-        print("Change Owed: " + abs(50 - total))
+        print("Change Owed: " + str(abs(50 - total)))
         break
 
