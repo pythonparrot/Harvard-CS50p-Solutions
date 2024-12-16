4e211b52a3136c775b19794c65197939b83193e7
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 19:07:25 +0000

    Mon, Dec 16, 2024, 2:07 PM -05:00

--- twttr.py
+++ twttr.py
@@ -1,15 +1,29 @@
 str = input("Input: ")
 str1 = str.lower()
 
-while True
+while True:
     if str1.find("a") != -1:
         i = str1.find("a")
         str = str[0 : i] + str[i + 1 : ]
         str1 = str[0 : i] + str[i + 1 : ]
     elif str1.find("e") != -1:
+        i = str1.find("e")
+        str = str[0 : i] + str[i + 1 : ]
+        str1 = str[0 : i] + str[i + 1 : ]
     elif str1.find("i") != -1:
+        i = str1.find("i")
+        str = str[0 : i] + str[i + 1 : ]
+        str1 = str[0 : i] + str[i + 1 : ]
     elif str1.find("o") != -1:
+        i = str1.find("o")
+        str = str[0 : i] + str[i + 1 : ]
+        str1 = str[0 : i] + str[i + 1 : ]
     elif str1.find("u") != -1:
+        i = str1.find("u")
+        str = str[0 : i] + str[i + 1 : ]
+        str1 = str[0 : i] + str[i + 1 : ]
     else:
         break
+
+print(str)
 
