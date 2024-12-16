6eca7ab1e2accd15705ac35b2f5a08a85b91b1fa
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 19:21:05 +0000

    Mon, Dec 16, 2024, 2:21 PM -05:00

--- twttr.py
+++ twttr.py
@@ -7,16 +7,17 @@ def has(string, letter):
         return false
 
 def removeVowel(string, letter):
-     string.find(letter.upper()) >= 0:
+    while string.find(letter.upper()) >= 0:
         string = string.remove(letter.upper())
-    elif string.find(letter.lower()) >= 0:
+    while string.find(letter.lower()) >= 0:
         string = string.remove(letter.lower())
+    return string
 
 
-while has(str, "a") or has(str, "e") or has(str, "i") or has(str, "o") or has(str, "u"):
+def main():
     if has(str, "a"):
-        str = str.remove("A")
-        str
-
-print(str)
+        removeVowel(str, "a")
+    if has(str, "e"):
+        removeVowel(str, "e")
+    if has (str, "i")
 
