dff033ccfe08bc0d3c31c20644141d0a4ab58129
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 19:04:59 +0000

    Mon, Dec 16, 2024, 2:04 PM -05:00

--- twttr.py
+++ twttr.py
@@ -3,7 +3,9 @@ str1 = str.lower()
 
 while True
     if str1.find("a") != -1:
-        str = str.remove("a")
+        i = str1.find("a")
+        str = str[0 : i] + str[i + 1 : ]
+        str1 = str[0 : i] + str[i + 1 : ]
     elif str1.find("e") != -1:
     elif str1.find("i") != -1:
     elif str1.find("o") != -1:
