ea191b33eb874efa78b6d545791ed7fa9c5265d5
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 19:03:05 +0000

    Mon, Dec 16, 2024, 2:03 PM -05:00

--- twttr.py
+++ twttr.py
@@ -1,3 +1,13 @@
 str = input("Input: ")
-while str.find("a") != -1 or str.find("i")
+str1 = str.lower()
+
+while True
+    if str1.find("a") != -1:
+        str = str.remove("a")
+    elif str1.find("e") != -1:
+    elif str1.find("i") != -1:
+    elif str1.find("o") != -1:
+    elif str1.find("u") != -1:
+    else:
+        break
 
