c2b38a835c83ab2662faf05d1b973314bf65fe8f
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 00:49:05 +0000

    Sun, Dec 15, 2024, 7:49 PM -05:00

--- interpreter.py
+++ interpreter.py
@@ -1,2 +1,13 @@
 x, y, z = input("Expression: ").split()
-print(float(x) / float(z))
+x = float(x)
+z = float(z)
+match(y):
+    case "+":
+        print(x + z)
+    case "-":
+        print(x - z)
+    case "*":
+        print(x * z)
+    case _:
+        print(x / z)
+
