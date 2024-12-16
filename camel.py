660a2d1054cd93f9ff1434728795dda0c40bca98
pythonparrot <vanillamoon210@gmail.com>
2024-12-16 18:30:49 +0000

    Mon, Dec 16, 2024, 1:30 PM -05:00

--- camel.py
+++ camel.py
@@ -7,6 +7,13 @@ for i in range(len(str)):
     if str[i].isupper():
         words.append(str[start : i])
         start = i
+    if i == len(str) - 1:
+        words.append(str[start : len(str)])
+
+finalStr = ""
+
+for word in words:
+    finalStr.append()
 
 print(words)
 
