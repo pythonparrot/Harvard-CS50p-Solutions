8db29229b28ddd080c2446e795d9f5348f1f1bc9
pythonparrot <vanillamoon210@gmail.com>
2024-12-17 18:20:00 +0000

    Tue, Dec 17, 2024, 1:20 PM -05:00

--- outdated.py
+++ outdated.py
@@ -1,6 +1,17 @@
-months = [
-    {"January": }
-]
+months = {
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
+}
 
 while True:
     try:
@@ -9,8 +20,10 @@ while True:
         break
     else:
         if date.find("/") > 0 and date.count("/") == 2:
-            x, y, z = date.split(sep = "/")
+            month, day, year = date.split(sep = "/")
+            if len(year) == 4 and (len(month) == 1 or len(month) == 2) and (len(day) == 1 or len(day) == 2):
+                if 
 
         elif date.find(" ") > 0 and date.count(" ") == 2:
-            x, y, z = date.split()
+            month, day, year = date.split()
 
