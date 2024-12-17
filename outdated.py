7c36de5772b5e342a43e05c22428fe90e236f46c
pythonparrot <vanillamoon210@gmail.com>
2024-12-17 18:47:49 +0000

    Tue, Dec 17, 2024, 1:47 PM -05:00

--- outdated.py
+++ outdated.py
@@ -21,19 +21,20 @@ while True:
     else:
         if date.count("/") == 2:
             month, day, year = date.split(sep = "/")
-            if len(year) == 4 and (len(month) == 1 or len(month) == 2) and (len(day) == 1 or len(day) == 2):
+            if len(year) == 4 and (len(month) == 1 or len(month) == 2) and (len(day) == 1 or len(day) == 2) and int(day) <= 31 and int(day) >= 1 and int(month) >= 1 and int(month) <= 12:
                 print(f"{year}-{int(month):02}-{int(day):02}")
                 break
         elif date.count(" ") == 2:
             month, day, year = date.split()
-            try:
-                month = months[month]
-            except KeyError:
-                pass
-            else:
-                day = day[0 : len(day) - 1]
-                print(f"{year}-{month}-{int(day):02}")
-                break
+            if int(day) <= 31 and int(day) >= 1 and int(month) >= 1 and int(month) <= 12:
+                try:
+                    month = months[month]
+                except KeyError:
+                    pass
+                else:
+                    day = day[0 : len(day) - 1]
+                    print(f"{year}-{month}-{int(day):02}")
+                    break
 
 
 
