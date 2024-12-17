85f036b68bffce99f322a5d8ed294883b0f9dcdb
pythonparrot <vanillamoon210@gmail.com>
2024-12-17 18:50:50 +0000

    Tue, Dec 17, 2024, 1:50 PM -05:00

--- outdated.py
+++ outdated.py
@@ -1,16 +1,16 @@
 months = {
-    "January": "01",
-    "February": "02",
-    "March": "03",
-    "April": "04",
-    "May": "05",
-    "June": "06",
-    "July": "07",
-    "August": "08",
-    "September": "09",
-    "October": "10",
-    "November": "11",
-    "December": "12"
+    "January": 1,
+    "February": 2,
+    "March": 3,
+    "April": 4,
+    "May": 5,
+    "June": 6,
+    "July": 7,
+    "August": 8,
+    "September": 9,
+    "October": 10,
+    "November": 11,
+    "December": 12
 }
 
 while True:
@@ -26,14 +26,14 @@ while True:
                 break
         elif date.count(" ") == 2:
             month, day, year = date.split()
-            if int(day) <= 31 and int(day) >= 1 and int(month) >= 1 and int(month) <= 12:
-                try:
-                    month = months[month]
-                except KeyError:
-                    pass
-                else:
-                    day = day[0 : len(day) - 1]
-                    print(f"{year}-{month}-{int(day):02}")
+            try:
+                month = months[month]
+            except KeyError:
+                pass
+            else:
+                day = day[0 : len(day) - 1]
+                if int(day) <= 31 and int(day) >= 1 and int(month) >= 1 and int(month) <= 12:
+                    print(f"{year}-{int(month):02}-{int(day):02}")
                     break
 
 
