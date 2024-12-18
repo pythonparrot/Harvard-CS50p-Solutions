c02e40dadeac27fe494bdff3bc59892da17eba4a
pythonparrot <vanillamoon210@gmail.com>
2024-12-18 18:34:59 +0000

    Wed, Dec 18, 2024, 1:34 PM -05:00

--- game.py
+++ game.py
@@ -1,8 +1,21 @@
 import random
 
 while True:
-    level = input("Level: )
-    if isinstance(level, int) and level > 0:
+    level = input("Level: ")
+    try:
+        if isinstance(int(level), int) and level > 0:
+            break
+    except:
+        
+
+num = random.randint(1, level)
+
+while True:
+    guess = input("Guess: ")
+    if guess < num:
+        print("Too small!")
+    if guess > num:
+        print("Too large!")
+    if guess == num:
+        print("Just right!")
         break
-
-
