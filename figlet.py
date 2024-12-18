0e3095a1df2a976628dbf5188e2fdfb28f2f9e1e
pythonparrot <vanillamoon210@gmail.com>
2024-12-18 16:48:07 +0000

    Wed, Dec 18, 2024, 11:48 AM -05:00

--- figlet.py
+++ figlet.py
@@ -1,8 +1,12 @@
 import pyfiglet
 import sys
 
+
+figlet = Figlet()
+fonts = figlet.getFonts()
+
 if len(sys.argv) == 1:
 
 elif len(sys.argv) == 3:
-    if (argv[1] != "-f" and argv[1] != "--font") or 
+    if (argv[1] != "-f" and argv[1] != "--font") or
 
