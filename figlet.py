eaef6e3ee072a904b87370fedb45d84c91d641f2
pythonparrot <vanillamoon210@gmail.com>
2024-12-18 17:20:26 +0000

    Wed, Dec 18, 2024, 12:20 PM -05:00

--- figlet.py
+++ figlet.py
@@ -7,16 +7,16 @@ figlet = Figlet()
 fonts = figlet.getFonts()
 
 if len(sys.argv) == 1:
-    str = input("Input: ")
+    user_input = input("Input: ")
     figlet.setFont(font = random.choice(fonts))
-    print(figlet.renderText(str))
+    print(figlet.renderText(user_input))
 elif len(sys.argv) == 3:
     if (sys.argv[1] != "-f" and sys.argv[1] != "--font") or sys.argv[2] not in fonts:
         sys.exit()
     else:
         figlet.setFont(font = sys.argv[2])
-        str = input("Input: ")
-        print(figlet.renderText(str))
+        user_input = input("Input: ")
+        print(figlet.renderText(user_input))
 else:
     sys.exit()
 
