e398ae995a9a123a4a16301f5e6dfb58e68db5c0
pythonparrot <vanillamoon210@gmail.com>
2024-12-19 15:51:01 +0000

    Thu, Dec 19, 2024, 10:51 AM -05:00

--- test_twttr.py
+++ test_twttr.py
@@ -1,1 +1,10 @@
+from twttr import shorten
 
+def test_twttr1():
+    assert shorten("Twitter") == "Twttr"
+
+def test_twttr2():
+    assert shorten("What's your name?") == "Wht's yr nm?"
+
+def test_twttr3():
+    assert shorten("CS50") == "CS50"
