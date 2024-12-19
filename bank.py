c97e076c6833f1513e754658ef9f56f9a6d09696
pythonparrot <vanillamoon210@gmail.com>
2024-12-19 16:22:24 +0000

    Thu, Dec 19, 2024, 11:22 AM -05:00

--- bank.py
+++ bank.py
@@ -1,15 +1,15 @@
 def main():
     g = input("Greeting: ")
-    print(value(g))
+    print(f"${value(g)}")
 
 def value(greeting):
     greeting = greeting.lower().strip()
     if greeting.find("hello") == 0:
-        return "$0"
+        return 0
     elif greeting.find("h") == 0:
-        return "$20"
+        return 20
     else:
-        return "$100"
+        return 100
 
 if __name__ == "__main__":
     main()
