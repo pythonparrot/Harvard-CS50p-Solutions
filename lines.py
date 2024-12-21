263194bffc26efb360fe17d72c361a908a47ed55
pythonparrot <vanillamoon210@gmail.com>
2024-12-21 00:18:20 +0000

    Fri, Dec 20, 2024, 7:18 PM -05:00

--- lines.py
+++ lines.py
@@ -1,9 +1,17 @@
 import sys
 
+count = 0
+
 if len(sys.argv) != 2:
     sys.exit("Error 1")
 else:
     if sys.argv[1].endswith(".py"):
-        
+        try:
+            with open(sys.argv[1]) as file:
+                for line in file:
+                    if line.lstrip().startswith("#"):
+                        sys.exit("Error 2")
+                    else:
+                        count += 1
     else:
         sys.exit("Error 3")
