23a429da0c0fb4b432d341f61cb2dbd00e781253
pythonparrot <vanillamoon210@gmail.com>
2024-12-21 00:49:43 +0000

    Fri, Dec 20, 2024, 7:49 PM -05:00

--- pizza.py
+++ pizza.py
@@ -4,5 +4,8 @@ import tabulate
 
 if len(sys.argv) != 2:
     sys.exit("Not right amount of command line arguments")
+elif sys.argv[1].endswith(".csv") != True
+    sys.exit("Invalid file name format")
 else:
-    
+    try:
+
