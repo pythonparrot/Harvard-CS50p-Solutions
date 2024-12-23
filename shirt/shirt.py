72f948a04ada01bbb9bee8f3a20206d29a712852
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 19:36:00 +0000

    Mon, Dec 23, 2024, 2:36 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,12 +1,17 @@
 import sys
 from PIL import Image
 from PIL import ImageOps
+import os
+
+
 
 if len(sys.argv) != 3:
     sys.exit("Invalid amount of command line arguments.")
 elif (not sys.argv[1].lower().endswith(".jpg", ".jpeg", ".png")) or (not sys.argv[2].lower().endswith(".jpg", ".jpeg", ".png")):
     sys.exit("Invalid file(s) format.")
-elif sys.argv[1][-2:] != sys.argv[2][-2:]:
+
+extension1 = os.path.splittext()
+if len(sys.argv[1]) - 2 != len(sys.argv[2]) != 2:
     sys.exit("Input extension doesn't match output extension.")
 else:
     try:
