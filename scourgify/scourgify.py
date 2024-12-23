f4971fc379674dcd863f49661d074bdadce36d59
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 18:17:58 +0000

    Mon, Dec 23, 2024, 1:17 PM -05:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,12 +5,11 @@ if len(sys.argv) != 3:
     sys.exit("Not exactly two command line arguments.")
 else:
     try:
-        with open(sys.argv[1], mode = "r") as file:
-            before_reader = csv.DictReader(file)
-        with open(sys.argv[2], mode = "w") as file:
-            after_writer = csv.DictWriter(file, fieldnames = ["first", "last", "house"])
+        with open(sys.argv[1], mode = "r") as bffile, open(sys.argv[2], mode = "w") as affile:
+            bf_reader = csv.DictReader(file)
+            af_writer = csv.DictWriter(file, fieldnames = ["first", "last", "house"])
+            for row in bf_reader:
+                last, first = row["name"].split(sep = ", ")
+                
     except FileNotFoundError:
         sys.exit("File not found.")
-    else:
-        for row in before_reader:
-            last, first = row["name"].split(sep = ", ")
