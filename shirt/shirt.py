cbfb2f9f39253e5c5e88581aa3b52846e643a71b
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 18:58:15 +0000

    Mon, Dec 23, 2024, 1:58 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,4 +1,5 @@
 import sys
+import Pillow
 
 if len(sys.argv) != 3:
     sys.exit("Invalid amount of command line arguments.")
@@ -8,6 +9,6 @@ elif sys.argv[1][-2:] != sys.argv[2][-2:]:
     sys.exit("Input extension doesn't match output extension.")
 else:
     try:
-        
+
     except FileNotFoundError:
         sys.exit("File not found.")
