90992ef9a5b47180aa2dd4a4e03bef417c9806dc
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 19:20:24 +0000

    Mon, Dec 23, 2024, 2:20 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -10,13 +10,13 @@ elif sys.argv[1][-2:] != sys.argv[2][-2:]:
     sys.exit("Input extension doesn't match output extension.")
 else:
     try:
-        pic1 = Image.open(sys.argv[1])
-        pic2 = Image.open(sys.argv[2])
-        shirt_pic = 
+        input_pic = Image.open(sys.argv[1])
+        output_pic = Image.open(sys.argv[2])
+        shirt_pic = Image.open("shirt.png")
     except FileNotFoundError:
         sys.exit("File not found.")
     else:
-        size = "shirt.png".size
-        resized_pic1 = ImageOps.fit(pic1, size))
+        size = shirt_pic.size
+        resized_input_pic = ImageOps.fit(input_pic, size)
 
 
