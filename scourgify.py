f4f9e44b45430b103d6c213ee23f54a0b81069ab
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 17:45:58 +0000

    Mon, Dec 23, 2024, 12:45 PM -05:00

---
+++ scourgify.py
@@ -0,0 +1,9 @@
+import sys
+
+if len(sys.argv) != 3:
+    sys.exit("Not exactly two command line arguments.")
+else:
+    try:
+        with open(sys.argv[1]) as file:
+    except FileNotFoundError:
+        sys.exit("File not found.")
