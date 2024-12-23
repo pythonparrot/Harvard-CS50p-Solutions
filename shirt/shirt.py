eb0bb9964f3d6587d79815328e8efcb30d8a7da1
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 18:56:35 +0000

    Mon, Dec 23, 2024, 1:56 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -4,4 +4,10 @@ if len(sys.argv) != 3:
     sys.exit("Invalid amount of command line arguments.")
 elif (not sys.argv[1].lower().endswith(".jpg", ".jpeg", ".png")) or (not sys.argv[2].lower().endswith(".jpg", ".jpeg", ".png")):
     sys.exit("Invalid file(s) format.")
-elif sys.argv[1][-2:] != sys.argv[2]
+elif sys.argv[1][-2:] != sys.argv[2][-2:]:
+    sys.exit("Input extension doesn't match output extension.")
+else:
+    try:
+        
+    except FileNotFoundError:
+        sys.exit("File not found.")
