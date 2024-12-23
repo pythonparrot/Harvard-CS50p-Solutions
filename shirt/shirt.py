88f472ea38801830dd3ab16bed259e92d8882319
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 18:54:47 +0000

    Mon, Dec 23, 2024, 1:54 PM -05:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,5 +1,7 @@
 import sys
 
 if len(sys.argv) != 3:
-    sys.exit("Not right amount of command line arguments.")
-elif (not sys.argv[1].lower().endswith(".jpg", ".jpeg", ".png")) or (not )
+    sys.exit("Invalid amount of command line arguments.")
+elif (not sys.argv[1].lower().endswith(".jpg", ".jpeg", ".png")) or (not sys.argv[2].lower().endswith(".jpg", ".jpeg", ".png")):
+    sys.exit("Invalid file(s) format.")
+elif sys.argv[1][-2:] != sys.argv[2]
