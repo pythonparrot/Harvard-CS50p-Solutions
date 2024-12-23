eb1c4e4f58e7bc291ab08a876464cbfaf3627dca
pythonparrot <vanillamoon210@gmail.com>
2024-12-23 17:35:16 +0000

    Mon, Dec 23, 2024, 12:35 PM -05:00

--- pizza.py
+++ pizza.py
@@ -10,6 +10,6 @@ else:
     try:
         with open(sys.argv[1]) as file:
             to_tabulate = list(csv.reader(file))
-            print(tabulate(to_tabulate, tablefmt = "grid"))
+            print(tabulate.tabulate(to_tabulate, tablefmt = "grid"))
     except FileNotFoundError:
         sys.exit("Specified file not found.")
