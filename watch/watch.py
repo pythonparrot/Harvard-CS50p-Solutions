9082edeb6a452d8dffdcbbf24d97c49db53ef05d
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 22:35:50 +0000

    Tue, Dec 24, 2024, 5:35 PM -05:00

--- watch/watch.py
+++ watch/watch.py
@@ -1,18 +1,14 @@
 import re
-# import sys
+import sys
 
 
-# def main():
-#     print(parse(input("HTML: ")))
+def main():
+    print(parse(input("HTML: ")))
 
 
-# def parse(s):
-#     return re.fullmatch(r'src="(https?://(?:www\.)?youtube\.com/embed/\w+"', s)
+def parse(s):
+    url = re.search(r'src="https?://(?:www\.)?(youtube\.com/embed/\w+)"', input)
+    return url.group(1)
 
-# if __name__ == "__main__":
-#     main()
-
-input = input("HTML: ")
-url = re.fullmatch(r'src="https?://(?:www\.)?(youtube\.com/embed/\w+)"', input)
-print(url.group(1))
-
+if __name__ == "__main__":
+    main()
