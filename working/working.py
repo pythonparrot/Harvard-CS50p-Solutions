725ab568a047241a8e6723c90606e81a732041ec
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 23:01:52 +0000

    Tue, Dec 24, 2024, 6:01 PM -05:00

--- working/working.py
+++ working/working.py
@@ -1,17 +1,23 @@
 import re
-import sys
+# import sys
 
 
-def main():
-    print(convert(input("Hours: ")))
+# def main():
+#     print(convert(input("Hours: ")))
 
 
-def convert(s):
-    if times := re.search(r"^[0-9]{1,2}(?::00)? (?:A|P)M to ", s)
+# def convert(s):
+#     if times := re.search(r"^[0-9]{1,2}(?::00)? (?:A|P)M to [0-9]{1,2}(?::00)? (?:A|P)M", s)
 
-    else:
-        raise ValueError
+#     else:
+#         raise ValueError
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+s = input("Hours: ")
+if re.search(r"^[0-9]{1,2}(?::00)? (?:A|P)M to [0-9]{1,2}(?::00)? (?:A|P)M", s):
+    print("True")
+else:
+    print("False")
