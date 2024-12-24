61c2498155b8092722973d3eff6be26e4e2adefc
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 22:00:07 +0000

    Tue, Dec 24, 2024, 5:00 PM -05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -8,7 +8,7 @@ def test_validate2():
     assert validate("255.255.255.255") == True
 
 def test_validate3():
-    assert validate("512.512.512.512") == False
+    assert validate("1.512.1.1") == False
 
 def test_validate4():
     assert validate("1.2.3.1000") == False
