36b06072d7ad034ac9997f25cabd266312bc2e08
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 21:43:45 +0000

    Tue, Dec 24, 2024, 4:43 PM -05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -6,7 +6,7 @@ def main():
     print(validate(input("IPv4 Address: ")))
 
 def validate(ip):
-    if matches := re.search(r"^([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)\.([0-9][0-9]?[0-9]?)$", ip):
+    if matches := re.search(r"^([0-9]{0-2})\.([0-9]{0-2})\.([0-9]{0-2})\.([0-9]{0-2})$", ip):
         for i in range(4):
             num = int(matches.group(i + 1))
             if num < 0 or num > 255:
