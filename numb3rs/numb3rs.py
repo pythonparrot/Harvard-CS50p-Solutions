eb09143f8980272c7f875921063e51f963878c55
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 21:30:40 +0000

    Tue, Dec 24, 2024, 4:30 PM -05:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -11,6 +11,8 @@ def validate(ip):
             num = int(matches.group(i + 1))
             if num < 0 or num > 255:
                 return "False"
+    else:
+        return "False"
     return "True"
 
 if __name__ == "__main__":
