1c4bdac089ec4e3307df5d202dc91fa6518995d3
pythonparrot <vanillamoon210@gmail.com>
2024-12-24 21:58:30 +0000

    Tue, Dec 24, 2024, 4:58 PM -05:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,19 +2,16 @@ import pytest
 from numb3rs import validate
 
 def test_validate1():
-    assert 1 == 1
+    assert validate("127.0.0.1") == True
 
-# def test_validate1():
-#     assert validate("127.0.0.1") == "True"
+def test_validate2():
+    assert validate("255.255.255.255") == True
 
-# def test_validate2():
-#     assert validate("255.255.255.255") == "True"
+def test_validate3():
+    assert validate("512.512.512.512") == False
 
-# # def test_validate3():
-# #     assert validate("512.512.512.512") == "False"
+def test_validate4():
+    assert validate("1.2.3.1000") == False
 
-# # def test_validate4():
-# #     assert validate("1.2.3.1000") == "False"
-
-# # def test_validate5():
-# #     assert validate("cat") == "False"
+def test_validate5():
+    assert validate("cat") == False
