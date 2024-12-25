64f075a66e6ab30efbcf86bbd8f9f8c8bb6763bf
pythonparrot <vanillamoon210@gmail.com>
2024-12-25 00:10:29 +0000

    Tue, Dec 24, 2024, 7:10 PM -05:00

--- working/working.py
+++ working/working.py
@@ -1,25 +1,27 @@
 import re
-# import sys
+import sys
 
 
-# def main():
-#     print(convert(input("Hours: ")))
+def main():
+    print(convert(input("Hours: ")))
 
 
-# def convert(s):
-#     if times := re.search(r"(^[0-9]{1,2}(?::[0-9]{1,2})? (?:A|P)M) to ([0-9]{1,2}(?::[0-9]{1,2})? (?:A|P)M)", s):
-#         print(times.group(1))
-#         print(times.group(2))
-#     else:
-#         raise ValueError
+def convert(s):
+    if times := re.search(r"(^(?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M) to ((?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M)", s):
+        time1_string1 = times.group(1)
+        time2_string1 = times.group(2)
+        if time1_string1.endswith("AM"):
+            
+    else:
+        raise ValueError
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
 
-s = input("Hours: ")
-if times := re.search(r"(^(?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M) to ((?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M)", s):
-    print(times.group(1))
-    print(times.group(2))
-else:
-    print("False")
+# s = input("Hours: ")
+# if times := re.search(r"(^(?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M) to ((?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M)", s):
+#     print(times.group(1))
+#     print(times.group(2))
+# else:
+#     print("False")
