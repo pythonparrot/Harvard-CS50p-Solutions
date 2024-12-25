4fe54f0d8f625a2ff7ed21bdc5a28b670a0ef918
pythonparrot <vanillamoon210@gmail.com>
2024-12-25 16:01:04 +0000

    Wed, Dec 25, 2024, 11:01 AM -05:00

--- response/response.py
+++ response/response.py
@@ -4,5 +4,7 @@ email1 = input("What's your email address?: ")
 try:
     if validators.email(email1):
         print("Valid")
+    else:
+        print("Invalid")
 except ValidationError:
     print("Invalid")
