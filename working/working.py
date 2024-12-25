11027f1d98eb96afa635d7ad208d790d945882de
pythonparrot <vanillamoon210@gmail.com>
2024-12-25 00:13:07 +0000

    Tue, Dec 24, 2024, 7:13 PM -05:00

--- working/working.py
+++ working/working.py
@@ -10,8 +10,7 @@ def convert(s):
     if times := re.search(r"(^(?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M) to ((?:[1-9]|[1][0-2])(?::[0-5][0-9])? (?:A|P)M)", s):
         time1_string1 = times.group(1)
         time2_string1 = times.group(2)
-        if time1_string1.endswith("AM"):
-            
+        if re.search(r"", time1_string1)
 
     else:
         raise ValueError
