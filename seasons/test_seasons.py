6287295ff8227867605b59b50230a193f62ce5e6
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 19:58:59 +0000

    Thu, Dec 26, 2024, 2:58 PM -05:00

--- seasons/test_seasons.py
+++ seasons/test_seasons.py
@@ -2,4 +2,4 @@ import pytest
 from seasons import convert
 
 def test_seasons1():
-    assert convert("2023-12-26") == "One million, fifty-one thousand, two hundred minutes"
+    assert convert("2023-12-26") == "One million, fifty-two thousand, six hundred forty minutes"
