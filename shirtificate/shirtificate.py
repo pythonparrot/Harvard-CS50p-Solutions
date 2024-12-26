f3ada226295ebaf141aa82f5a129ad1a5b22826c
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 21:55:24 +0000

    Thu, Dec 26, 2024, 4:55 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,8 +1,18 @@
 from fpdf import FPDF
 
-name = input("Name: ")
+class PDF(FPDF):
+    def header(self):
+        # Setting font: helvetica bold 15
+        self.set_font("helvetica", style="B", size=50)
+        # Moving cursor to the right:
+        self.cell(80)
+        # Printing title:
+        self.cell(30, 10, "CS50 Shirtificate", align="C")
+        # Performing a line break:
+        self.ln(20)
 
-pdf = FPDF()
+#name = input("Name: ")
+pdf = PDF()
 pdf.add_page()
 pdf.image("shirtificate.png", w = 185, x = 10, y = 70)
 pdf.output("shirtificate.pdf")
