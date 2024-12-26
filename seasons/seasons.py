f4e80cfa4e160eb0cde1ae340a9d01e0eadd9b10
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 19:33:16 +0000

    Thu, Dec 26, 2024, 2:33 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -5,7 +5,7 @@ import inflect
 
 def main():
     date = input("Date of birth: ")
-    if re.search(r"[0-9][]", date)
+    if re.search(r"^[0-9][0-9][0-9][0-9]-(?:[0][1-9]|[1][0-2])-(?:[0-2][0-9]|[3][0-1])$", date)
 
 
 if __name__ == "__main__":
