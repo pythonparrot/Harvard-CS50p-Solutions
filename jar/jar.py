01cc49bc26cbb4504234e62b3fed115e7cc53be6
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 20:21:05 +0000

    Thu, Dec 26, 2024, 3:21 PM -05:00

--- jar/jar.py
+++ jar/jar.py
@@ -7,18 +7,27 @@ class Jar:
             raise ValueError
 
     def __str__(self):
-        ...
+        to_return = ""
+        for i in range(self.size):
+            to_return = to_return + "🍪"
+        return to_return
 
     def deposit(self, n):
-        ...
+        if self.size + n <= self.capacity:
+            self.size += n
+        else:
+            raise ValueError
 
     def withdraw(self, n):
-        ...
+        if self.size - n >= 0:
+            self.size -= n
+        else:
+            raise ValueError
 
     @property
     def capacity(self):
-        ...
+        return self.capacity
 
     @property
     def size(self):
-        ...
+        return self.size
--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,5 +1,22 @@
-import jar
+from jar import Jar
 
-def test_jar1():
-    
 
+def test_init():
+    ...
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    ...
+
+
+def test_withdraw():
+    ...
