ce8baf977d26d18f8d569c7030e27e29cb22ff10
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 21:49:31 +0000

    Thu, Dec 26, 2024, 4:49 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,5 +2,5 @@ from fpdf import FPDF
 
 pdf = FPDF()
 pdf.add_page()
-pdf.image("shirtificate.png", x = pdf.eph/2, h = pdf.epw/2)
+pdf.image("shirtificate.png", w = 20, keep_aspect_ratio=True)
 pdf.output("shirtificate.pdf")
