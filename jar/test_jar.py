e2a6498d6f5788632ae9f50bd43044c4b04c0729
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 20:53:39 +0000

    Thu, Dec 26, 2024, 3:53 PM -05:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -1,22 +1,37 @@
 from jar import Jar
-
+import pytest
 
 def test_init():
-    ...
+    jar = Jar(3)
+    assert jar._capacity == 3
+
+    jar1 = Jar(32)
+    assert jar1._capacity == 32
 
 
 def test_str():
     jar = Jar()
     assert str(jar) == ""
-    # jar.deposit(1)
-    # assert str(jar) == "🍪"
-    # jar.deposit(11)
-    # assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+    jar.deposit(3)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
 
 def test_deposit():
-    ...
+    jar = Jar()
+    jar.deposit(1)
+    assert str(jar) == "🍪🍪🍪"
+    jar.deposit(4)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪"
+    with pytest.raises(ValueError):
+        jar.deposit(10)
 
 
 def test_withdraw():
-    ...
+    jar = Jar()
+    jar.deposit(5)
+    jar.withdraw(2)
+    assert str(jar) == "🍪🍪🍪"
+    with pytest.raises(ValueError):
+        jar.withdraw(4)
