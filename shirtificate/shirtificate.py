bde283fe743f5ad6b5a72eb9227cdc1793350b95
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 21:45:11 +0000

    Thu, Dec 26, 2024, 4:45 PM -05:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -2,6 +2,5 @@ from fpdf import FPDF
 
 pdf = FPDF()
 pdf.add_page()
-pdf.set_font("helvetica", style="B", size=16)
-pdf.cell(40, 10, "Hello World!")
-pdf.output("tuto1.pdf")
+pdf.image("shirtificate.png")
+pdf.output("shirtificate.pdf")
