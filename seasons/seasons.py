e5ed78aac4ba52b3c309c55ee83738769299747d
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 19:37:34 +0000

    Thu, Dec 26, 2024, 2:37 PM -05:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -4,8 +4,16 @@ import inflect
 
 
 def main():
-    date = input("Date of birth: ")
-    if re.search(r"^[0-9][0-9][0-9][0-9]-(?:[0][1-9]|[1][0-2])-(?:[0-2][0-9]|[3][0-1])$", date)
+    date1 = input("Date of birth: ")
+    if groups := re.search(r"^([0-9][0-9][0-9][0-9])-([0][1-9]|[1][0-2])-([0-2][0-9]|[3][0-1])$", date1):
+        try:
+            date2 = date(groups[1], groups[2], groups[3])
+        except ValueError:
+            sys.exit("Invalid date")
+        else:
+            
+    else:
+        sys.exit("Invalid date")
 
 
 if __name__ == "__main__":
