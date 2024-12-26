ff3d44134f70aaae25a6e6270b8ac36ee8265bc6
pythonparrot <vanillamoon210@gmail.com>
2024-12-26 17:59:12 +0000

    Thu, Dec 26, 2024, 12:59 PM -05:00

--- week8/hat.py
+++ week8/hat.py
@@ -1,6 +1,11 @@
+import random
+
 class Hat:
+    def __init__(self):
+        self.houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
     def sort(self, name):
-        print(f"{name} is in some house")
+        print(f"{name} is in {random.choice(self.houses)}")
 
 
 hat = Hat()
