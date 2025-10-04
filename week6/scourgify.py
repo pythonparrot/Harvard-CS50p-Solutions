1d82f87e4e484b4da093f4089c525f6998488795
Andrew <vanillamoon210@gmail.com>
2025-10-04 11:48:46 -0400

    Remove duplicate scourgify.py

--- week6/scourgify.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-if len(sys.argv) != 3:
-    sys.exit("Not exactly two command line arguments.")
-else:
-    try:
-        with open(sys.argv[1]) as file:
-    except FileNotFoundError:
-        sys.exit("File not found.")
